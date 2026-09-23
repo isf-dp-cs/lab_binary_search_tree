87fdfec07a244244a817ef734c2660362ce66041
ms. brown <eqbrown@isf.edu.hk>
2026-09-23 16:23:49 +0800

    add todo items

--- search.py
+++ search.py
@@ -26,36 +26,9 @@ def create_bst(words, balanced):
     return bst
 
 
-def search_time_linear(words, search_word):
-    """Returns time it takes to find a word
-    using a linear search in a list"""
-
-    start = time.time()
-    for word in words:
-        if word == search_word:
-            end = time.time()
-    end = time.time()
-    return end - start
-
-
-def search_time_bst(words, search_word):
-    """Returns time it takes to find a word
-    using searing a BST"""
-
-    start = time.time()
-    words.search("zoo")
-    end = time.time()
-    return end - start
-
-
 if __name__ == "__main__":
     words = create_word_list("words_100k.txt")
-    search_word = "lunch"
-    linear_time = search_time_linear(words, search_word)
     bst = create_bst(words, True)
-    print(linear_time)
-    bst_time = search_time_bst(bst, search_word)
-    print(f"{linear_time:.10f}")
 
-    print(f"{bst_time:.10f}")
-    print(linear_time > bst_time)
+    # TODO: Compare the search times between the linear list
+    # and the BST balanced and the BST unbalanced
